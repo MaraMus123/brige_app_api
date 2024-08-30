cb34f84ef86be8a86a384f9be9181a074058f5f0
Marek Musinsky <marek.musinsky@gmail.com>
2024-08-30 13:20:49 +0200

    Changes has been made

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y together, return result"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y return result"""
+    return y - x
