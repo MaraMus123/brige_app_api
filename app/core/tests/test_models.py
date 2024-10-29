2c392b26c8ed8e0e8e8c93a1daaaf147015a8ff0
Marek Musinsky <marek.musinsky@gmail.com>
2024-10-29 21:49:12 +0100

    Implemented the user app

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Tests for modules
+Tests for models.
 """
 
 from django.test import TestCase  # type: ignore
@@ -28,7 +28,7 @@ class ModelTest(TestCase):
         sample_emails = [
             ["test1@EXAMPLE.com", "test1@example.com"],
             ["Test2@Example.com", "Test2@example.com"],
-            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
             ["test4@example.COM", "test4@example.com"],
         ]
         for email, expected in sample_emails:
