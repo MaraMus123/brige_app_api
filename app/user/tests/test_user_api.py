7379a8a71b8eb7d50d0e16a43a4189e1ac42166e
Marek Musinsky <marek.musinsky@gmail.com>
2024-10-30 00:20:22 +0100

    Problem fixed

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -16,11 +16,11 @@ def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class PublicUserAPiTests(TestCase):
+class PublicUserApiTests(TestCase):
     """Test the public features of the user API"""
 
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
 
     def test_create_user_success(self):
         """Test creating a user is successful"""
@@ -55,11 +55,10 @@ class PublicUserAPiTests(TestCase):
             'password': 'test',
             'name': 'Test Name',
         }
-        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.fiter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
